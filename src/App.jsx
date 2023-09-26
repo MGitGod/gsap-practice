e18e6c655c52eeeccd5b3a7612bb14a8866d3591
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-09-26 16:26:02 +0300

    feat: add scrolltrigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,55 @@
-import logo from "./logo.svg";
+import React, { useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import dog1 from "./dog-1.jpg";
+import dog2 from "./dog-2.jpg";
+import dog3 from "./dog-3.jpg";
 import "./App.css";
 
 function App() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  useLayoutEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".section--blue",
+        start: "top top",
+        end: "bottom -=100%",
+        scrub: true,
+        pin: true,
+        toggleClass: "square--active",
+        markers: {
+          startColor: "white",
+          endColor: "white",
+        },
+      },
+    });
+    tl.fromTo(
+      ".dog-1",
+      { opacity: 0, x: -500 },
+      { opacity: 1, x: 0, rotation: 25 }
+    )
+      .fromTo(
+        ".dog-2",
+        { opacity: 0, x: 500 },
+        { opacity: 1, x: 0, rotation: -25 }
+      )
+      .fromTo(
+        ".dog-3",
+        { opacity: 0, y: -500 },
+        { opacity: 1, y: 0, rotation: 20 }
+      );
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Типичный веб разработчик</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React + GSAP Scrolltrigger
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section section--green"></section>
+      <section className="section section--blue">
+        <img src={dog1} alt="" className="dog-1" />
+        <img src={dog2} alt="" className="dog-2" />
+        <img src={dog3} alt="" className="dog-3" />
+      </section>
+      <section className="section section--green"></section>
+    </>
   );
 }
 
